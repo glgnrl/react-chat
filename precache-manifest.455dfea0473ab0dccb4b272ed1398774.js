00ff360510bb8836c7c8476bb6a840ea5906a9b0
Gilang Nuril Haqim <gilangn89@gmail.com>
2019-01-17 08:59:25 +0800

    Updates

---
+++ precache-manifest.455dfea0473ab0dccb4b272ed1398774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c79d0569fe4b36a81978",
+    "url": "/react-chat/static/js/runtime~main.c79d0569.js"
+  },
+  {
+    "revision": "851fc0459f6267f258bb",
+    "url": "/react-chat/static/js/main.851fc045.chunk.js"
+  },
+  {
+    "revision": "99f694b9216b8328faca",
+    "url": "/react-chat/static/js/1.99f694b9.chunk.js"
+  },
+  {
+    "revision": "851fc0459f6267f258bb",
+    "url": "/react-chat/static/css/main.1ae1a67a.chunk.css"
+  },
+  {
+    "revision": "d26e0944db9e2f6eb4e245ca9dbea5fb",
+    "url": "/react-chat/index.html"
+  }
+];
